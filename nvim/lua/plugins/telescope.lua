b89f380dd55066bd522eb8b31b8c15218902d386
duma799 <gptduma@gmail.com>
2026-02-12 01:10:48 +0400

    Move Telescope setup() to main spec, make search function local

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,37 +4,6 @@ return {
         tag = "0.1.8",
         dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
-            local builtin = require("telescope.builtin")
-            vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-            vim.keymap.set("n", "<A-f>", builtin.live_grep, {})
-
-            -- Function for searching and scoping into a directory
-            function search_and_scope_into_directory()
-                builtin.find_files({
-                    prompt_title = "Search Directories",
-                    find_command = {"fd", "--type", "d", "--hidden", "--follow"},
-                    attach_mappings = function(prompt_bufnr, map)
-                        local actions = require("telescope.actions")
-                        actions.select_default:replace(function()
-                            local selection = require("telescope.actions.state").get_selected_entry()
-                            local selected_dir = selection.path
-
-                            -- Change the current working directory to the selected directory
-                            vim.cmd("cd " .. selected_dir)
-                            actions.close(prompt_bufnr)
-                        end)
-                        return true
-                    end,
-                })
-            end
-
-            -- Keybinding to trigger directory search and scoping
-            vim.keymap.set("n", "<A-d>", search_and_scope_into_directory)
-        end
-    },
-    {
-        "nvim-telescope/telescope-ui-select.nvim",
-        config = function()
             require("telescope").setup({
                 extensions = {
                     ["ui-select"] = {
@@ -47,13 +16,13 @@ return {
                     },
                     vimgrep_arguments = {
                         "rg",
-                        "--follow",        
-                        "--hidden",        
-                        "--no-heading",    
+                        "--follow",
+                        "--hidden",
+                        "--no-heading",
                         "--with-filename",
                         "--line-number",
-                        "--column",        
-                        "--smart-case",    
+                        "--column",
+                        "--smart-case",
                         "--glob=!**/.git/*",
                         "--glob=!**/.idea/*",
                         "--glob=!**/.vscode/*",
@@ -63,7 +32,6 @@ return {
                         "--glob=!**/package-lock.json",
                     },
                 },
-
                 pickers = {
                     find_files = {
                         hidden = true,
@@ -82,8 +50,37 @@ return {
                     },
                 },
             })
+
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+            vim.keymap.set("n", "<A-f>", builtin.live_grep, {})
+
+            local function search_and_scope_into_directory()
+                builtin.find_files({
+                    prompt_title = "Search Directories",
+                    find_command = {"fd", "--type", "d", "--hidden", "--follow"},
+                    attach_mappings = function(prompt_bufnr, map)
+                        local actions = require("telescope.actions")
+                        actions.select_default:replace(function()
+                            local selection = require("telescope.actions.state").get_selected_entry()
+                            local selected_dir = selection.path
+                            vim.cmd("cd " .. selected_dir)
+                            actions.close(prompt_bufnr)
+                        end)
+                        return true
+                    end,
+                })
+            end
+
+            -- Expose globally for alpha.lua dashboard button
+            _G.search_and_scope_into_directory = search_and_scope_into_directory
+            vim.keymap.set("n", "<A-d>", search_and_scope_into_directory)
+        end
+    },
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        config = function()
             require("telescope").load_extension("ui-select")
         end,
     },
 }
-
